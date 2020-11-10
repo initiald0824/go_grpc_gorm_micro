0b978e43ec3438f12bb74ca9d60320f9207b1e73
chenfuhua <2498168786@qq.com>
2020-11-10 16:42:01 +0800

    重来

--- curd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-read tableName
-cd cmd/
-go run main.go curd -t ${tableName}
--- lib/constant/constant.go
+++
@@ -1,7 +0,0 @@
-package constant
-
-const (
-	CONFIGFILE = "/Users/zl/go/src/my/go_grpc_gorm_micro/config.yaml" // 默认配置文件
-	PAGE = 1 // 当前页面
-	PAGESIZE = 20 // 每页大小
-)
